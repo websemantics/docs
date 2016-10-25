3e0aa60a0559b7e8c45689f74e8e8a2ab39d0a18
Adnan <adnan@websemantics.ca>
2016-10-24 23:48:44 -0400

    Add js folder

---
+++ docs/assets/js/docs.js
@@ -0,0 +1,50 @@
+/**
+ *                                   /'\
+ *                                  /   \
+ *                                 /     \
+ *               _                /   ..' \_                      /\/'\
+ *              / \              / './       \                   /     \
+ *            _/   \            /             \        _        / oO    \
+ *  /'\      /  '._'\          /               \      / \      /      /''\
+ * /   \    /        \        /      /          \    /   \    / \_ ..'    \
+ *      \  /          \__    /      /            \  /     \__/             \  
+ *       \/              \  /      /              \/__     \                \  
+ *        \               \/    __/                   \     \                \     
+ *         \              /                            \     \                     
+ *                                ))))                        \__                        
+ *   _________ _______     __ooO_(O o)_Ooo____                                  
+ *  |          \      \   /     | (_)         \\===========\===========\         
+ *  |     __    \      \ /      |     __      |            |           |           
+ *  |    |__)    \      V      /|    |__)     |            |           |            
+ *  |            |\           / |            <            ┌┴┐         ┌┴┐
+ *  |        ___/  |         |  |             \          _ \ \       /  |
+ *  |       |      |         |  |      |\      \        | \_| |     |    \_
+ *  |       |      |         |  |      | \      \        \___/  __  \      `-_
+ *  |_______|      |_________|  |______|  |______|             /   \  ''-.    \
+ *   ____   __ __  ____  _     ___      ___  ____             /    /      \    \
+ *  |    \ |  |  ||    || |   |   \    /  _||    \            |   |        |   |
+ *  | ()  )|  |  | |  | | |\\\\    \  /  |_ | ()  )           \    \      /    /
+ *  |     ||  |  | |  | |  ( oo) () ||    _||    /             \    `-..-'    /
+ *  | ()  ||  :  | |  | |   (_)     ||   |_ |    \       - --   '-_        _-'
+ *  |_____| \__,_||____||____||_____||_____||__|\_|                `------'
+ *  Addons scaffolder for maximum coding pleasure            - -- --------- -- -
+ *
+ *  This project was released under MIT license.
+ *
+ *  @link      http://websemantics.ca
+ *  @author    Web Semantics, Inc. Dev Team <team@websemantics.ca>
+ *  @author    Adnan M.Sagar, PhD. <adnan@websemantics.ca>
+ *
+ */
+
+!function ($) {
+  'use strict'
+
+  $(function () {
+
+    $(".language-diagram").sequenceDiagram({theme: 'simple'})
+    $(".language-chart").flowChart({'line-width': 2})
+
+  })
+
+}(jQuery)
